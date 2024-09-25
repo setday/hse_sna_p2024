b8aebf92db5ece31529202592b3f285fb36eb473
Daniil <danila-masterok@mail.ru>
2024-09-25 17:33:30 +0300

    fix

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -14,10 +14,8 @@ files = [
     # "Votes",
 ]
 
-def pl_read_xml(file: str, datapath: str = "data/") -> pd.DataFrame:
-    pd_df = pd.read_xml(f"{datapath}{file}.xml")
-    # pl_df = pl.DataFrame(pd_df)
-    return pd_df
+def read_xml(file: str, datapath: str = "data/") -> pd.DataFrame:
+    return pd.read_xml(f"{datapath}{file}.xml")
 
 def load_dataset(datapath: str = "data/") -> dict[str, pd.DataFrame]:
     if not os.path.exists(datapath):
@@ -25,13 +23,7 @@ def load_dataset(datapath: str = "data/") -> dict[str, pd.DataFrame]:
         os.system("bash utils/load_data.sh")
 
     data = {}
-
-    for file in files:
-        if not os.path.exists(f"{datapath}{file}.xml"):
-            print(f"File {file}.xml not found. Loading data from XML files.")
-            os.system(f"bash utils/load_{file}.sh")
-        
-        data[file] = pl_read_xml(file, datapath)
+    for file in files:        
+        data[file] = read_xml(file, datapath)
         print(f"Loaded {file}.xml")
-
     return data
