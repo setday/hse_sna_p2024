2e6553fcef3be9eecab5f4b43f8d2ad4529b54e3
Nikolay <nskripko@icloud.com>
2024-09-27 12:18:54 +0300

    Add file for embedders

---
+++ embedders_comparison/question_to_tags/embedders.py
@@ -0,0 +1,8 @@
+EMBEDDERS = {
+    'Albert': 'paraphrase-albert-small-v2',
+    'Roberta': 'all-distilroberta-v1',
+    'DistilBert': 'multi-qa-distilbert-cos-v1',
+    'MiniLM1': 'all-MiniLM-L6-v2',
+    'MiniLM2': 'all-MiniLM-L12-v2',
+    'MiniLM3': 'paraphrase-MiniLM-L3-v2'
+}
