ee6061d075422eccb5ae2bd65c86a9f018b76d18
Alexander Serkov <32331578+setday@users.noreply.github.com>
2024-12-22 04:52:38 +0300

    Fix graph building issue (connected with tags)

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def load_dataset(filepath: str = "../data/Posts.xml") -> pd.DataFrame:
+def load_dataset(filepath: str = "../data/Posts.xml", full: bool = False) -> pd.DataFrame:
     posts = pd.read_xml(filepath, parser="etree")
     print("Data loaded")
-    return posts[:10000]
+    if not full:
+        return posts[:10000]
+    return posts
