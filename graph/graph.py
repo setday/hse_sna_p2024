37a69d7327f39614302ef6a3e5c1265eec4b6b11
Artem Batygin <aa.batygin@gmail.com>
2024-12-24 20:47:53 +0300

    Add baseline

--- graph/graph.py
+++ graph/graph.py
@@ -5,8 +5,10 @@ import pandas
 import networkx
 import igraph
 import matplotlib.pyplot
+import pickle
 
 sys.path.append("../complexity_hunters/")  # to make utils importable
+sys.path.append(".")  # to make utils importable
 sys.path.append("..")  # to make utils importable
 
 import utils.data_worker
@@ -137,31 +139,8 @@ def add_undefined_atributes(graph):
             graph.nodes[node]["score"] = 0
 
 
-def build_partition(graph):
-    i_graph = igraph.Graph.from_networkx(graph)
-    partition = i_graph.community_leiden(
-        objective_function="modularity", n_iterations=1000
-    )
-
-    node_to_community = {
-        node: partition.membership[i] for i, node in enumerate(i_graph.vs["_nx_name"])
-    }
-    communities = [set() for _ in range(max(partition.membership) + 1)]
-    for node in i_graph.vs["_nx_name"]:
-        communities[node_to_community[node]].add(node)
-
-    return node_to_community, communities
-
-
-def build_graph():
+def build_graph(show=False):
     posts = utils.data_worker.load_dataset(utils.consts.POSTS_DATA_PATH, debug_slice=True)
-    # questions, answers = utils.data_worker.question_answer_split(posts)
-    # posts = pandas.concat(
-    #     [
-    #         questions.sample(n=500).reset_index(drop=True),
-    #         answers.sample(n=10000).reset_index(drop=True),
-    #     ]
-    # )
     badges = utils.data_worker.load_dataset(utils.consts.BADGES_DATA_PATH, debug_slice=False)
     badges = badges[badges.UserId.isin(posts.OwnerUserId.unique())]
 
@@ -170,7 +149,7 @@ def build_graph():
 
     posts = utils.data_worker.posts_fill_na(posts)
 
-    users = numpy.sort(posts.OwnerUserId.unique())[1:500]  # remove NaN
+    users = numpy.sort(posts.OwnerUserId.unique())[1:]  # remove NaN
     questions, answers = utils.data_worker.question_answer_split(posts)
 
     graph = networkx.Graph()
@@ -187,62 +166,19 @@ def build_graph():
 
     add_undefined_atributes(graph)
 
-    user_nodes = [node for node in graph.nodes if graph.nodes[node]["type"] == "user"]
-    users_to_community, user_communities = build_partition(
-        graph.subgraph(user_nodes)
-    )
-    
-    user_stereotypes = []
-    for community in user_communities:
-        tags = set()
-        for user in community:
-            tags |= set(graph.nodes[user]["tags"])
+    if show:
+        pos = {}
+        for i, user in enumerate(user_nodes):
+            x = i / len(user_nodes)
+            pos[user] = (x * (1 - x), x)
 
-        rates = {tag: 0 for tag in tags}
-        for user in community:
-            for tag in graph.nodes[user]["tags"]:
-                rates[tag] += 1
+        for i, question in enumerate(question_nodes):
+            x = i / len(question_nodes)
+            pos[question] = (1 - x * (1 - x), x)
 
-        for key in rates.keys():
-            rates[key] /= len(community)
+        networkx.draw(graph, pos, node_size=20, width=1, alpha=0.1)
+        matplotlib.pyplot.show()
 
-        user_stereotypes.append(rates)
+    pickle.dump(graph, open("../data/graph.pkl", "wb"))
+    print("INFO: Dumped graph into ./data/graph.pkl")
 
-
-    question_nodes = [node for node in graph.nodes if graph.nodes[node]["type"] == "question"]
-    question_to_community, question_communities = build_partition(
-        graph.subgraph(question_nodes)
-    )
-
-    question_stereotypes = []
-    for community in question_communities:
-        tags = set()
-        for question in community:
-            tags |= set(graph.nodes[question]["tags"])
-
-        rates = {tag: 0 for tag in tags}
-        for question in community:
-            for tag in graph.nodes[question]["tags"]:
-                rates[tag] += 1
-
-        for key in rates.keys():
-            rates[key] /= len(community)
-
-        question_stereotypes.append(rates)
-
-
-    pos = {}
-    for i, user in enumerate(user_nodes):
-        x = i / len(user_nodes)
-        pos[user] = (x * (1 - x), x)
-
-    for i, question in enumerate(question_nodes):
-        x = i / len(question_nodes)
-        pos[question] = (1 - x * (1 - x), x)
-
-    networkx.draw(graph, pos, node_size=20, width=1, alpha=0.1)
-    matplotlib.pyplot.show()
-
-
-if __name__ == "__main__":
-    build_graph()
